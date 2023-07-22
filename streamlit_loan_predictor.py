cbdd2362958d08681c3603d2503e258831082d9f
ChungEun Jennifer Chae <cchae216@gmail.com>
2023-07-22 13:57:17 -0400

    fixing

--- streamlit_loan_predictor.py
+++ streamlit_loan_predictor.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+from sklearn.svm import SVC
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 
 
 import loan_data
