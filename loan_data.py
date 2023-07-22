e60c08c783a9afa1d10ccb2b8dd9c6152f15a85a
ChungEun Jennifer Chae <cchae216@gmail.com>
2023-07-22 10:20:37 -0400

    remove matplotlib

--- loan_data.py
+++ loan_data.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.svm import SVC
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
--- streamlit_loan_predictor.py
+++ streamlit_loan_predictor.py
@@ -1,7 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+
+from sklearn.svm import SVC
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+
 import loan_data
+
 # A predictive model (SVC) to predict the loan status of an applicant.
 @st.cache_data
 def get_fvalue(val):
