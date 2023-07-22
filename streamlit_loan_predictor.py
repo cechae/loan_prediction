377d50477a84bcbfdf1e7d5afd4fffda2a8e4df0
ChungEun Jennifer Chae <cchae216@gmail.com>
2023-07-22 10:23:28 -0400

    remove sklearn from main

--- streamlit_loan_predictor.py
+++ streamlit_loan_predictor.py
@@ -2,9 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-from sklearn.svm import SVC
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
+
 
 import loan_data
 
